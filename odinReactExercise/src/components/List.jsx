41bbc38b9938fea8bc96cf8949910a8433e01743
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-04-15 18:32:55 +0100

    Added data.jsx

--- odinReactExercise/src/components/List.jsx
+++ odinReactExercise/src/components/List.jsx
@@ -1,16 +1,14 @@
 function List({todos}) {
     if (!todos) {
-        console.log("Empty todos...")
         return <p>Loading...</p>
     }
 
     if (todos.length === 0) {
-        console.log("todos length = 0...")
         return <p>No items yet. Why not add some?</p>
     }
 
-    const listItems = todos.map((todo, index) => (
-        <li key={index}>{todo}</li>
+    const listItems = todos.map((todo) => (
+        <li key={crypto.randomUUID()}>{todo}</li>
     ));
     return (
         <ul>
